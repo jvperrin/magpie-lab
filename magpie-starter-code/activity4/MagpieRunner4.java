9f8726239b7042f686113189cadbe053c6b70c4b
Jason Perrin <jasonvperrin@gmail.com>
2014-09-26 21:06:35 +0000

    Finish activity 4

--- magpie-starter-code/activity4/MagpieRunner4.java
+++ magpie-starter-code/activity4/MagpieRunner4.java
@@ -5,25 +5,21 @@ import java.util.Scanner;
  * @author Laurie White
  * @version April 2012
  */
-public class MagpieRunner4
-{
+public class MagpieRunner4 {
 
 	/**
 	 * Create a Magpie, give it user input, and print its replies.
 	 */
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		Magpie4 maggie = new Magpie4();
-		
+
 		System.out.println (maggie.getGreeting());
 		Scanner in = new Scanner (System.in);
 		String statement = in.nextLine();
-		
-		while (!statement.equals("Bye"))
-		{
+
+		while (!statement.equals("Bye")) {
 			System.out.println (maggie.getResponse(statement));
 			statement = in.nextLine();
 		}
 	}
-
 }
