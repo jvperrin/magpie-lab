145858a47c50a9807ff0c3528bcee55aadf058f3
Jason Perrin <jasonvperrin@gmail.com>
2014-09-27 19:22:53 +0000

    Finish activity 5

--- magpie-starter-code/activity5/Magpie5.java
+++ magpie-starter-code/activity5/Magpie5.java
@@ -4,7 +4,7 @@ import java.util.Random;
  * A program to carry on conversations with a human user.
  * This version:
  *<ul><li>
- * 		Uses advanced search for keywords 
+ * 		Uses advanced search for keywords
  *</li><li>
  * 		Will transform statements as well as react to keywords
  *</li></ul>
@@ -15,17 +15,17 @@ import java.util.Random;
 public class Magpie5
 {
 	/**
-	 * Get a default greeting 	
+	 * Get a default greeting
 	 * @return a greeting
-	 */	
+	 */
 	public String getGreeting()
 	{
 		return "Hello, let's talk.";
 	}
-	
+
 	/**
 	 * Gives a response to a user statement
-	 * 
+	 *
 	 * @param statement
 	 *            the user statement
 	 * @return a response based on the rules given
@@ -48,6 +48,9 @@ public class Magpie5
 				|| findKeyword(statement, "brother") >= 0)
 		{
 			response = "Tell me more about your family.";
+		}
+		else if (findKeyword(statement, "song name") >= 0) {
+			response = "Darude - Sandstorm";
 		}
 
 		// Responses which require transformations
@@ -93,9 +96,9 @@ public class Magpie5
 		}
 		return response;
 	}
-	
+
 	/**
-	 * Take a statement with "I want to <something>." and transform it into 
+	 * Take a statement with "I want to <something>." and transform it into
 	 * "What would it mean to <something>?"
 	 * @param statement the user statement, assumed to contain "I want to"
 	 * @return the transformed statement
@@ -116,9 +119,9 @@ public class Magpie5
 		return "What would it mean to " + restOfStatement + "?";
 	}
 
-	
+
 	/**
-	 * Take a statement with "I want <something>." and transform it into 
+	 * Take a statement with "I want <something>." and transform it into
 	 * "Would you really be happy if you had <something>?"
 	 * @param statement the user statement, assumed to contain "I want"
 	 * @return the transformed statement
@@ -138,9 +141,9 @@ public class Magpie5
 		String restOfStatement = statement.substring(psn + 6).trim();
 		return "Would you really be happy if you had " + restOfStatement + "?";
 	}
-	
+
 	/**
-	 * Take a statement with "you <something> me" and transform it into 
+	 * Take a statement with "you <something> me" and transform it into
 	 * "What makes you think that I <something> you?"
 	 * @param statement the user statement, assumed to contain "you" followed by "me"
 	 * @return the transformed statement
@@ -156,16 +159,16 @@ public class Magpie5
 			statement = statement.substring(0, statement
 					.length() - 1);
 		}
-		
+
 		int psnOfYou = findKeyword (statement, "you", 0);
 		int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
-		
+
 		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
 		return "What makes you think that I " + restOfStatement + " you?";
 	}
-	
+
 	/**
-	 * Take a statement with "I <something> you" and transform it into 
+	 * Take a statement with "I <something> you" and transform it into
 	 * "Why do you <something> me?"
 	 * @param statement the user statement, assumed to contain "I" followed by "you"
 	 * @return the transformed statement
@@ -181,21 +184,21 @@ public class Magpie5
 			statement = statement.substring(0, statement
 					.length() - 1);
 		}
-		
+
 		int psnOfI = findKeyword (statement, "I", 0);
 		int psnOfYou = findKeyword (statement, "you", psnOfI);
-		
+
 		String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
 		return "Why do you " + restOfStatement + " me?";
 	}
-	
 
-	
-	
+
+
+
 	/**
 	 * Search for one word in phrase.  The search is not case sensitive.
 	 * This method will check that the given goal is not a substring of a longer string
-	 * (so, for example, "I know" does not contain "no").  
+	 * (so, for example, "I know" does not contain "no").
 	 * @param statement the string to search
 	 * @param goal the string to search for
 	 * @param startPos the character of the string to begin the search at
@@ -206,12 +209,12 @@ public class Magpie5
 		String phrase = statement.trim();
 		//  The only change to incorporate the startPos is in the line below
 		int psn = phrase.toLowerCase().indexOf(goal.toLowerCase(), startPos);
-		
-		//  Refinement--make sure the goal isn't part of a word 
-		while (psn >= 0) 
+
+		//  Refinement--make sure the goal isn't part of a word
+		while (psn >= 0)
 		{
 			//  Find the string of length 1 before and after the word
-			String before = " ", after = " "; 
+			String before = " ", after = " ";
 			if (psn > 0)
 			{
 				before = phrase.substring (psn - 1, psn).toLowerCase();
@@ -220,51 +223,55 @@ public class Magpie5
 			{
 				after = phrase.substring(psn + goal.length(), psn + goal.length() + 1).toLowerCase();
 			}
-			
+
 			//  If before and after aren't letters, we've found the word
 			if (((before.compareTo ("a") < 0 ) || (before.compareTo("z") > 0))  //  before is not a letter
 					&& ((after.compareTo ("a") < 0 ) || (after.compareTo("z") > 0)))
 			{
 				return psn;
 			}
-			
+
 			//  The last position didn't work, so let's find the next, if there is one.
 			psn = phrase.indexOf(goal.toLowerCase(), psn + 1);
-			
+
 		}
-		
+
 		return -1;
 	}
-	
+
 	/**
 	 * Search for one word in phrase.  The search is not case sensitive.
 	 * This method will check that the given goal is not a substring of a longer string
-	 * (so, for example, "I know" does not contain "no").  The search begins at the beginning of the string.  
+	 * (so, for example, "I know" does not contain "no").  The search begins at the beginning of the string.
 	 * @param statement the string to search
 	 * @param goal the string to search for
 	 * @return the index of the first occurrence of goal in statement or -1 if it's not found
 	 */
-	private int findKeyword(String statement, String goal)
-	{
+	private int findKeyword(String statement, String goal) {
 		return findKeyword (statement, goal, 0);
 	}
-	
+
 
 
 	/**
 	 * Pick a default response to use if nothing else fits.
 	 * @return a non-committal string
 	 */
-	private String getRandomResponse ()
-	{
+	private String getRandomResponse () {
 		Random r = new Random ();
 		return randomResponses [r.nextInt(randomResponses.length)];
 	}
-	
-	private String [] randomResponses = {"Interesting, tell me more",
-			"Hmmm.",
-			"Do you really think so?",
-			"You don't say."
+
+	private String [] randomResponses = {
+		"Interesting, tell me more",
+		"Hmmm.",
+		"Do you really think so?",
+		"You don't say.",
+		"I WILL EAT YOUR RABBITS.",
+		"wat",
+		"Could you repeat that?",
+		"Why are you talking to me anyway?",
+		"YOUR POULTRY WILL BE MINE."
 	};
-	
+
 }
